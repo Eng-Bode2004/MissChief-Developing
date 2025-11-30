b56045d6312f8fdfb0672279ea784000a131e762
abdelrahman-ashraf <zezomodebode@gmail.com>
2025-12-01 00:26:50 +0200

    init commit

---
+++ Controllers/UserControllers.ts
@@ -0,0 +1,36 @@
+import type {Request, Response} from "express";
+import UserServices from "../Services/UserServices";
+
+class UserControllers {
+
+    async createUser(req: Request, res: Response) {
+        try {
+            const userData = req.body;
+            const newUser = await UserServices.createUser(userData);
+
+            return res.status(201).json({
+                message: "User created successfully",
+                user: newUser,
+                status: 201
+            });
+
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                res.status(400).json({
+                    message: error.message,
+                    status: 400
+                });
+            } else {
+                res.status(400).json({
+                    message: "Unknown error occurred",
+                    status: 400
+                });
+            }
+        }
+    }
+
+
+
+}
+
+export default new UserControllers();
