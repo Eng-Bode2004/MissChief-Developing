45824e4e920658fb6af5d01f0b34ac293531f99d
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-12-05 23:45:15 +0200

    init commit

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import "./Config/DataBase.js";
+import cors from "cors";
 import AZ_Routes from "./Routes/AZ_Routes.ts";
 
 // Initialize Express
@@ -8,6 +9,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET","POST","PUT","DELETE","PATCH"],
+    credentials: true
+}));
 
 // Routes
 app.use("/api/v1/az", AZ_Routes);
