306ac3dc2cc628df7df924a11e89c544fe69bf4b
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-12-08 16:55:07 +0200

    init commit

--- Controllers/ImagesControllers.js
+++ Controllers/ImagesControllers.js
@@ -11,9 +11,9 @@ class ImagesControllers{
             }
 
             const imageData = {
-                URL: req.file.secure_url,
-                title: req.body.title ?? req.file.originalname,
-                description: req.body.description ?? ""
+                URL: req.file?.secure_url || req.file?.path,
+                title: req.body?.title || req.file?.originalname || "No-title",
+                description: req.body?.description || ""
             };
 
             const savedImage = await ImageServices.uploadPhoto(imageData);
